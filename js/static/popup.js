59251dcb816aef4edfe70377a8a08092539da5f1
冯天祥 <751728440@qq.com>
2018-02-11 16:11:04 +0800

    2018年2月11日16:10:59

---
+++ js/static/popup.js
@@ -0,0 +1,44 @@
+$(function() {
+
+    'use strict';
+    
+    var $message = $('#message');
+
+    var optionHtml = '';
+    for (var i = 1; i <= 500; i++) {
+        optionHtml += '<option value="' + i + '">前' + i + '页</option>';
+    }
+
+    var $pageCount = $('#page-count');
+    $pageCount.append(optionHtml).val('50');
+
+    var showMessage = function(message, color) {
+        $message.html(message).css({
+            color: color
+        }).show();
+
+        window.setTimeout(function() {
+            $message.hide();
+        }, 3000);
+    };
+
+    $('#get-customers').click(function() {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function(tabs) {
+            var tab = tabs[0];
+            if (tab.url.indexOf('.weibo.com/') < 0) {
+                showMessage('请先打开<a href="https://s.weibo.com" target="_blank">微博搜索</a>页面！', 'red');
+                return;
+            }
+
+            chrome.tabs.sendMessage(tab.id, {
+                method: 'fetchCustomers',
+                pageCount: parseInt($pageCount.val())
+            }, function(response) {
+                showMessage('正在提取用户...', 'green');
+            });
+        });
+    });
+});
